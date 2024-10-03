0000ce0c1aa5e2d96eb676c7afb7d515242243e8
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-02 22:02:44 -0500

    added missing categoryId in DTO class

--- e-commerce/src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ e-commerce/src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class ProductDTO {
 
     private Long productId;
+    private Long categoryId;
     private String productName;
     private String description;
     private String image;
