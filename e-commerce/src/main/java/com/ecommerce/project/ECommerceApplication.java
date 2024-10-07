0308b067974e4f46ee29ba043cf9728a0392040d
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-06 20:11:45 -0500

    JWT and Spring Security

--- e-commerce/src/main/java/com/ecommerce/project/ECommerceApplication.java
+++ e-commerce/src/main/java/com/ecommerce/project/ECommerceApplication.java
@@ -8,7 +8,7 @@ public class ECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
-		System.out.println("App is Up and Running");
+		System.err.println("E-commerce App is Up and Running");
 	}
 
 }
