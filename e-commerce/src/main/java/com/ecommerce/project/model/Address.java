d7dc1f2dc9a9de9f898aa0ec82532cc44c1cf3f2
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-07 11:54:25 -0500

    Loosely coupled Entities

--- e-commerce/src/main/java/com/ecommerce/project/model/Address.java
+++ e-commerce/src/main/java/com/ecommerce/project/model/Address.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +42,7 @@ public class Address {
     @Size(min = 3, message = "Country name must be atleast 3 characters")
     private String country;
 
+    @ToString.Exclude
     @ManyToMany(mappedBy = "addresses")
     private List<User> users = new ArrayList<>();
 
